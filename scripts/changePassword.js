65641d5966f72c434c67bcce12d9db395f2debac
yaarslv <savenkovyaroslav53@gmail.com>
2024-02-18 20:40:03 +0300

    delete all

--- scripts/changePassword.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector('.change-password').addEventListener('submit', function (e) {
-        e.preventDefault();
-        window.location.href = 'forgot-password.html?redirect=profile.html'
-    });
-});
